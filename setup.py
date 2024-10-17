248fe36b73c94af4369d1ca64f2e7791c67a9fdb
aerithnetzer <aerithnetzer@outlook.com>
2024-10-17 15:31:51 -0500

    edit setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
+import os
+
+# Read the long description from the README file
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="ztkn",
     version="0.1.0",
     description="CLI Tool for Zettelkasten Graph Visualization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Specify the content type
     author="Aerith Netzer",
     author_email="your.email@example.com",
     packages=find_packages(),
